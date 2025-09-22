2163fe2a29ecec25ef50645f0ad233eb6dbce4eb
andre <andre@DESKTOP-1PTS1RO>
2025-09-22 07:22:04 -0300

    Att app previsao do tempo, erros diferentes adicionados

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,25 +1,67 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+using System.Net;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                    if (t != null)
+                    {
+                        string dados_previsao =
+                            $"Lat: {t.lat}\n" +
+                            $"Lon: {t.lon}\n" +
+                            $"Temp. Min: {t.temp_min} ºC\n" +
+                            $"Temp. Max: {t.temp_max} ºC\n" +
+                            $"Visibilidade: {t.visibility} m\n" +
+                            $"Velocidade do Vento: {t.speed} m/s\n" +
+                            $"Clima: {t.main}\n" +
+                            $"Descrição: {t.description}\n" +
+                            $"Nascer do Sol: {t.sunrise}\n" +
+                            $"Pôr do Sol: {t.sunset}\n";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        lbl_res.Text = dados_previsao;
+                    }
+                    else 
+                    {
+                        await DisplayAlert("Cidade não encontrada",
+                            $"Não localizamos \"{txt_cidade.Text}\". Verifique o nome e tente novamente.",
+                            "Ok"); // Mensagem de erro se a cidade não for encontrada
+                    }
+                }
+                else
+                {
+                    lbl_res.Text = "Informe uma cidade válida";// Mensagem de erro se o campo estiver vazio
+                }
+            }
+            catch (HttpRequestException) 
+            {
+                await DisplayAlert("Sem conexão",
+                    "Não foi possível conectar. Verifique sua internet e tente novamente.",
+                    "Ok"); // Mensagem de erro se não houver conexão com a internet
+            }
+            catch (WebException webEx) when ((webEx.Response as HttpWebResponse)?.StatusCode == HttpStatusCode.NotFound) //
+            {
+                await DisplayAlert("Cidade não encontrada",
+                    $"A cidade \"{txt_cidade.Text}\" não foi localizada.",
+                    "Ok");             }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro inesperado", ex.Message, "OK");
+            } // Mensagem de erro geral
         }
     }
-
 }
---
+++ Services/DataService.cs
@@ -0,0 +1,54 @@
+﻿using MauiAppTempoAgora.Models;
+using Newtonsoft.Json.Linq;
+using System.Text.Json.Nodes;
+
+namespace MauiAppTempoAgora.Services
+{
+    internal class DataService
+    {
+       public static async Task<Tempo?> GetPrevisao(string cidade)
+        {
+            Tempo? t = null;
+
+            string chave = "f23d755c5cc93b7b7c11b5e59cc62378";
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather" +
+                $"?q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync(); //
+                    var rascunho = JObject.Parse(json); //
+
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]?["sunrise"]).ToLocalTime(); 
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]?["sunset"]).ToLocalTime();
+
+                    t = new()
+                    {
+
+                        lat = rascunho["coord"]?["lat"]?.Value<double>() ?? 0,      
+                        lon = rascunho["coord"]?["lon"]?.Value<double>() ?? 0,
+                        description = rascunho["weather"]?[0]?["description"]?.Value<string>() ?? "",
+                        main = rascunho["weather"]?[0]?["main"]?.Value<string>() ?? "",
+                        temp_min = rascunho["main"]?["temp_min"]?.Value<double>() ?? 0,
+                        temp_max = rascunho["main"]?["temp_max"]?.Value<double>() ?? 0,
+                        visibility = rascunho["visibility"]?.Value<int>() ?? 0,
+                        speed = rascunho["wind"]?["speed"]?.Value<double>() ?? 0,
+                        sunrise = sunrise.ToString("HH:mm"),
+                        sunset = sunset.ToString("HH:mm")
+
+                    }; // fechamento do new Tempo{
+
+                }// fechamento do if (resp.IsSuccessStatusCode){
+            } // fechamento do using (HttpClient client = new HttpClient()){
+
+
+            return t;
+        }// fechamento do public static async Task<Tempo?> GetPrevisao(string cidade){
+    }// fechamento do namespace MauiAppTempoAgora.Services
+}// fechamento do namespace MauiAppTempoAgora.Services
